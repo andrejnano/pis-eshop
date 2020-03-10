21a1bc6d4e7c55cadad38c1c85ebeed65200c92b
Andrej Nano <andrejn26@gmail.com>
2020-03-11 00:41:59 +0100

    comment deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script is used to deploy the whole system inside a production environment
+# It is envoked by a 'Webhook' service, which is running on a specified hostname:port
+# configured in the project's github repository. When a 'git push' operation occurs,
+# Github communicates with the 'Webhook' service and launches this script in production.
+
 git fetch --all
 git checkout --force "origin/master"
 make clean
